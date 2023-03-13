47c1d42fe3fa7d6661442e7ca11c2544ad17dacb
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-13 21:47:10 +0100

    fixed layout + add array for mail

--- js/script.js
+++ js/script.js
@@ -9,20 +9,20 @@ Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
+//DADI
 const dice = [1,2,3,4,5,6];
 const btnDice = document.getElementById("diceBtn");
-
 btnDice.addEventListener("click", rollDice);
 
 function rollDice (){
-    const score = document.querySelector("h6");
+    const score = document.querySelector(".print-dice");
     if (score) score.remove();
     const playerScore = dice[Math.floor(Math.random() * dice.length)];
     const cpuScore = dice[Math.floor(Math.random() * dice.length)];
-    const scorePrint = document.createElement("h6");
-    scorePrint.classList.add("text-white");
-    const diceCol = document.getElementById("diceCol");
-    diceCol.append(scorePrint)
+    const scorePrint = document.createElement("div");
+    scorePrint.classList.add("text-white", "h6", "print-dice");
+    const diceCard = document.getElementById("diceCard");
+    diceCard.append(scorePrint);
     
     if (playerScore > cpuScore){
         scorePrint.innerHTML = `HAI VINTO! Il tuo punteggio è ${playerScore}, quello della CPU è ${cpuScore}`;
@@ -31,7 +31,19 @@ function rollDice (){
         scorePrint.innerHTML = `HAI PERSO! Il tuo punteggio è ${playerScore}, quello della CPU è ${cpuScore}`;
     }
     else {
-        scorePrint.innerHTML = `PAREGGIO! Il tuo punteggio è ${playerScore}, quello della CPU è ${cpuScore}`
+        scorePrint.innerHTML = `PAREGGIO! Il tuo punteggio è ${playerScore}, quello della CPU è ${cpuScore}`;
     }
 }
 
+//MAIL
+const fakeEmail = [
+    "sono.un.tutor@possoaccedere.it",
+    "sono.clelia@possoaccedere.it",
+    "come.ti.permetti@possoaccedere.it",
+    "certo.che@possoaccedere.it",
+    "mario.rossi@possoaccedere.it",
+    "giulia.verdi@possoaccedere.it",
+    "sara.bianchi@possoaccedere.it"
+];
+
+const mailBtn = document.getElementById("mailBtn");
