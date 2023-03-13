d2e6ac1bfcaf72b5b1afe51c54d927148b36a713
Dan848 <daniele.ceravolo15@gmail.com>
2023-03-13 23:12:44 +0100

    add fuction to mail button

--- js/script.js
+++ js/script.js
@@ -20,17 +20,20 @@ function rollDice (){
     const playerScore = dice[Math.floor(Math.random() * dice.length)];
     const cpuScore = dice[Math.floor(Math.random() * dice.length)];
     const scorePrint = document.createElement("div");
-    scorePrint.classList.add("text-white", "h6", "print-dice");
+    scorePrint.classList.add("h6", "print-dice", "rounded-2");
     const diceCard = document.getElementById("diceCard");
     diceCard.append(scorePrint);
     
     if (playerScore > cpuScore){
+        scorePrint.classList.add("text-bg-warning");
         scorePrint.innerHTML = `HAI VINTO! Il tuo punteggio è ${playerScore}, quello della CPU è ${cpuScore}`;
     }
     else if (playerScore < cpuScore){
+        scorePrint.classList.add("text-bg-danger");
         scorePrint.innerHTML = `HAI PERSO! Il tuo punteggio è ${playerScore}, quello della CPU è ${cpuScore}`;
     }
     else {
+        scorePrint.classList.add("text-white");
         scorePrint.innerHTML = `PAREGGIO! Il tuo punteggio è ${playerScore}, quello della CPU è ${cpuScore}`;
     }
 }
@@ -46,4 +49,30 @@ const fakeEmail = [
     "sara.bianchi@possoaccedere.it"
 ];
 
-const mailBtn = document.getElementById("mailBtn");
+const btnMail = document.getElementById("mailBtn");
+btnMail.addEventListener("click", checkMail);
+
+function checkMail () {
+    const userMail = document.getElementById("userMail").value;
+    const print = document.querySelector(".print-mail");
+    if (print) print.remove();
+    const mailPrint = document.createElement("div");
+    mailPrint.classList.add("h6", "print-mail", "rounded-2");
+    const mailCard = document.getElementById("mailCard");
+    mailCard.append(mailPrint);
+    let access = false;
+
+    for (let i = 0; i < fakeEmail.length - 1; i++) {
+        if (userMail.toLowerCase() === fakeEmail[i].toLowerCase()){
+            access = true;
+        }
+    }
+    if (access) {
+        mailPrint.classList.add("text-bg-warning");
+        mailPrint.innerHTML = `La tua email è nel nostro elenco, puoi accedere`;
+    }
+    else {
+        mailPrint.classList.add("text-bg-danger");
+        mailPrint.innerHTML = `Chi ti conosce? Nessuno`;
+    }
+}
